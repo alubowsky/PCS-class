9f9ac12646d9bdc0a971737b006c5a0dc4781d71
alubowsky <zevelmail13@gmail.com>
2017-09-16 22:56:30 -0500

    homework

---
+++ Javascript/HW_74/javascript.js
@@ -0,0 +1,20 @@
+window.alert(name);
+
+function greaterThan(x, y){
+    if(x > y){
+        return true;
+    }
+    else if(x < y){
+        return false;
+    }
+    else{
+        return console.log("they are equal");
+    }
+
+    //console.log(greaterThan(5,4));
+
+    //onsole.log(greaterThan(4,5));
+
+    //console.log(greaterThan(5,5));
+
+}
