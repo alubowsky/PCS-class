fa87dcca21fa0b70495d2d3869c33b52de5eb1b3
alubowsky <zevelmail13@gmail.com>
2018-07-09 09:27:01 -0500

    fix up

--- Javascript/HW_101/app101/src/app/app.module.ts
+++ Javascript/HW_101/app101/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { CategoryComponent } from './category/category.component';
@@ -12,7 +12,8 @@ import { CategoryComponent } from './category/category.component';
     CategoryComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
